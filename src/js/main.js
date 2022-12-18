a112af1f0e49e48c9f6f842dd3780d14bb2b35c2
vsulimovvv <super.sulimov2014@yandex.ru>
2022-12-18 15:51:43 +0300

    src

---
+++ src/js/main.js
@@ -0,0 +1,123 @@
+window.addEventListener('DOMContentLoaded', () => {
+  //   // * ===== Mask input
+  //   $('input[type="tel"]').mask('+7 (999) 999-99-99');
+
+  // * ===== Nice Select
+  $('select').niceSelect();
+
+  // * ===== loadForm
+  (function loadForm() {
+    let inputs = document.querySelectorAll('.input-file-upload');
+    Array.prototype.forEach.call(inputs, function (input) {
+      let label = input.previousElementSibling,
+        labelVal = label.querySelector('.custom-file-upload span').innerText;
+
+      input.addEventListener('change', function (e) {
+        let countFiles = '';
+        if (this.files && this.files.length >= 1)
+          countFiles = this.files.length;
+
+        if (countFiles)
+          label.querySelector('.custom-file-upload span').innerText =
+            'Выбрано файлов: ' + countFiles;
+        else
+          label.querySelector('.custom-file-upload span').innerText = labelVal;
+      });
+    });
+  })();
+
+  // * ===== Slider
+  (function slider() {
+    const sliderEl = document.querySelector('.cabinet__slider');
+    new Swiper(sliderEl, {
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      slidesPerView: 'auto',
+      spaceBetween: 15,
+    });
+  })();
+
+  // * ==== Dropdown
+  document.addEventListener('click', (e) => {
+    const isDropdownButton = e.target.matches('[data-dropdown-button]');
+    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null)
+      return;
+
+    let currentDropdown;
+    if (isDropdownButton) {
+      currentDropdown = e.target.closest('[data-dropdown]');
+      currentDropdown.classList.toggle('active');
+    }
+
+    document.querySelectorAll('[data-dropdown].active').forEach((dropdown) => {
+      if (dropdown === currentDropdown) return;
+      dropdown.classList.remove('active');
+    });
+  });
+
+  // * ===== Fixed Header
+  (function fixedHeader() {
+    function scrollHeader() {
+      const nav = document.querySelector('header');
+      if (this.scrollY >= 90) {
+        nav.classList.add('scroll-header');
+      } else {
+        nav.classList.remove('scroll-header');
+      }
+    }
+
+    window.addEventListener('scroll', scrollHeader);
+
+    // ! Change
+    function changeBg() {
+      const header = document.querySelector('header');
+      if (window.pageYOffset >= 90) {
+        header.classList.add('scroll-header');
+      }
+    }
+
+    changeBg();
+  })();
+
+  // * ===== Modal
+  (function modals() {
+    function bindModal(openBtn, modal, close) {
+      const openBtnEl = document.querySelectorAll(openBtn);
+      const modalEl = document.querySelector(modal);
+      const closeEl = document.querySelectorAll(close);
+      const body = document.querySelector('body');
+      if (modalEl) {
+        openBtnEl.forEach((el) => {
+          el.addEventListener('click', (e) => {
+            if (e.target) {
+              e.preventDefault();
+            }
+            modalEl.classList.add('active');
+            body.classList.add('no-scroll');
+          });
+        });
+        closeEl.forEach((btn) => {
+          btn.addEventListener('click', (e) => {
+            modalEl.classList.remove('active');
+            body.classList.remove('no-scroll');
+          });
+        });
+        modalEl.addEventListener('click', (e) => {
+          if (e.target === modalEl) {
+            modalEl.classList.remove('active');
+            body.classList.remove('no-scroll');
+          }
+        });
+      }
+    }
+    bindModal('.btn-submit', '.popup--send', '.popup__close');
+    bindModal('.btn-create-left', '.popup--create-left', '.popup__close');
+    bindModal('.btn-create-right', '.popup--create-right', '.popup__close');
+    bindModal('.btn-start-game', '.popup--start-game', '.popup__close');
+    bindModal('.btn-report', '.popup--report', '.popup__close');
+    bindModal('.btn-fill-data', '.popup--fill-data', '.popup__close');
+    bindModal('.btn-data-transfers', '.popup--data-transfers', '.popup__close');
+    bindModal('.player-btn', '.popup--info-block', '.popup__close');
+  })();
+});
